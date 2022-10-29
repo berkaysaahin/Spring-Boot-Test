a9808e868fad8593def456e55be88a94bd13d8c5
berkaysaahin <berkaynihas@gmail.com>
2022-10-29 20:54:36 +0300

    ResultResponse

--- src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -1,10 +1,11 @@
 package kodlamaio.northwind.business.abstracts;
 
+import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.entities.concretes.Product;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public interface ProductService {
-    List<Product> getAll();
+    DataResult<List<Product>> getAll();
 }
